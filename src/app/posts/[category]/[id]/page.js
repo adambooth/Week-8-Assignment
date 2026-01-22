809045545cbae8a7d92edad26d33916f9a91ce24
Adam Booth <adam_booth@live.co.uk>
2026-01-22 17:20:03 +0000

    added liking and deleting

--- src/app/posts/[category]/[id]/page.js
+++ src/app/posts/[category]/[id]/page.js
@@ -1,8 +1,10 @@
 // import db
 import { db } from "@/utils/.dbConnection";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export default async function posts({ params }) {
-  const { id } = await params;
+  const { id, category } = await params;
 
   const { rows } = await db.query(`SELECT * FROM week7posts WHERE id = $1`, [
     id,
@@ -16,10 +18,49 @@ export default async function posts({ params }) {
     return <h1>Post not found</h1>;
   }
 
+  const handleLike = async () => {
+    "use server";
+    await db.query(
+      `UPDATE week7posts SET likecount = likecount + 1 WHERE id = $1 RETURNING *`,
+      [post.id],
+    );
+
+    revalidatePath("/");
+
+    redirect(`/posts/category/${post.id}`);
+  };
+
+  const handleDelete = async () => {
+    "use server";
+    await db.query(`DELETE FROM week7posts WHERE id = $1`, [post.id]);
+    revalidatePath("/");
+
+    redirect("/");
+  };
+
   return (
     <>
       <h1>Id : {post.id}</h1>
       <h1>Name : {post.creator}</h1>
+      <div className="like-delete-container">
+        <p className="likecount"> Likes : {post.likecount}</p>
+        <button className="like-delete-btn" onClick={handleLike}>
+          <img
+            width="50"
+            height="50"
+            src="https://img.icons8.com/pastel-glyph/64/facebook-like--v1.png"
+            alt="facebook-like--v1"
+          />
+        </button>
+        <button className="like-delete-btn" onClick={handleDelete}>
+          <img
+            width="50"
+            height="50"
+            src="https://img.icons8.com/carbon-copy/50/filled-trash.png"
+            alt="filled-trash"
+          />
+        </button>
+      </div>
     </>
   );
 }
--- src/components/ShowAllPosts.jsx
+++ src/components/ShowAllPosts.jsx
@@ -24,25 +24,6 @@ export default async function ShowAllPosts() {
                 <p className="post-category post-content">
                   Category: {posts.category}
                 </p>
-                <div className="like-delete-container">
-                  <p className="likecount"> Likes : {posts.likecount}</p>
-                  <button className="like-delete-btn">
-                    <img
-                      width="50"
-                      height="50"
-                      src="https://img.icons8.com/pastel-glyph/64/facebook-like--v1.png"
-                      alt="facebook-like--v1"
-                    />
-                  </button>
-                  <button className="like-delete-btn">
-                    <img
-                      width="50"
-                      height="50"
-                      src="https://img.icons8.com/carbon-copy/50/filled-trash.png"
-                      alt="filled-trash"
-                    />
-                  </button>
-                </div>
               </Link>
             </div>
           );
