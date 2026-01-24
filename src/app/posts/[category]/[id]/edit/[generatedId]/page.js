fa9d8c1231d71f57eec7b8cf15b75703653f0a10
Adam Booth <adam_booth@live.co.uk>
2026-01-24 11:09:31 +0000

    added editing comments

--- src/app/posts/[category]/[id]/edit/[generatedId]/page.js
+++ src/app/posts/[category]/[id]/edit/[generatedId]/page.js
@@ -1,11 +1,10 @@
 import { db } from "@/utils/.dbConnection";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import "./editCommentPage.css";
 
 export default async function editCommentPage({ params }) {
   const { id: postId, generatedId: commentId } = await params;
-
-  console.log("params:", params);
 
   const CommentId = parseInt(commentId, 10);
 
@@ -21,35 +20,34 @@ export default async function editCommentPage({ params }) {
   }
 
   return (
-    <div>
-      <h1>Edit Comment</h1>
-      <form
-        action={async (formData) => {
-          "use server";
-          const updatedName = formData.get("name");
-          const updatedComment = formData.get("comment");
+    <div className="edit-comment-container">
+      <div className="form-wrapper">
+        <h1>Edit Comment</h1>
+        <form
+          action={async (formData) => {
+            "use server";
+            const updatedName = formData.get("name");
+            const updatedComment = formData.get("comment");
 
-          await db.query(
-            `UPDATE week7postscomments SET name = $1, comment = $2 WHERE generatedid = $3`,
-            [updatedName, updatedComment, CommentId],
-          );
+            await db.query(
+              `UPDATE week7postscomments SET name = $1, comment = $2 WHERE generatedid = $3`,
+              [updatedName, updatedComment, CommentId],
+            );
 
-          revalidatePath(`/posts/category/${postId}`);
-          redirect(`/posts/category/${postId}`);
-        }}
-      >
-        <div>
+            revalidatePath(`/posts/category/${postId}`);
+            redirect(`/posts/category/${postId}`);
+          }}
+        >
           <label>Name:</label>
           <input type="text" name="name" defaultValue={comment.name} required />
-        </div>
-
-        <div>
           <label>Comment:</label>
           <textarea name="comment" defaultValue={comment.comment} required />
-        </div>
 
-        <button type="submit">Save</button>
-      </form>
+          <button className="submit-button" type="submit">
+            Save
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
